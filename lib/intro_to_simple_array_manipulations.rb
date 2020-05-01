9419ec465036f19bc683500ec40542929aa906a2
Brook Beyene <brookbeyene37@gmail.com>
2020-05-01 04:58:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,10 +3,16 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, new_element)
-  array.insert(4, new_element)
+  return array.insert(4, new_element)
 
 end
 
 def using_uniq(array)
-  array.uniq
+  return array.uniq
 end 
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def
